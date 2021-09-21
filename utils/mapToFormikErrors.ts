514e839fb19fdadff064b7a474e009f470b54a3a
prajotsurey <prajotsurey@gmail.com>
2021-09-21 16:15:09 +0530

    comments

--- utils/mapToFormikErrors.ts
+++ utils/mapToFormikErrors.ts
@@ -1,6 +1,7 @@
 import { ValidateOutput } from "../generated/graphql"
 
 const mapToFormikErrors = (errors: ValidateOutput[]) => {
+  //maps the server errors to errors which formik can deal with
   const map: Record<string, string> = {}
   errors.map(e => {
     map[e.field] = e.message
