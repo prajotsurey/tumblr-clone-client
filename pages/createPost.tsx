998e4c6f2dfc33b60d89ecf8ab7a8a8f01fc0ce3
prajotsurey <prajotsurey@gmail.com>
2021-09-27 23:16:24 +0530

    create post also displays dashboard in the background

--- pages/createPost.tsx
+++ pages/createPost.tsx
@@ -1,16 +1,19 @@
 import { Formik, Form } from 'formik';
-import router from 'next/dist/client/router';
+import router, { useRouter } from 'next/dist/client/router';
 import React, { useEffect } from 'react'
 import { setAccessToken, getAccessToken } from '../accessToken';
 import { CreatePostForm } from '../components/createPostForm';
 import { CustomButton } from '../components/CustomButton';
 import { FormErrorSection } from '../components/FormErrorSection';
 import { InputField } from '../components/InputField';
-import { useCreatePostMutation, useMeQuery } from '../generated/graphql';
+import { useCreatePostMutation, useMeQuery, usePostsQuery } from '../generated/graphql';
 import mapToFormikErrors from '../utils/mapToFormikErrors';
 import withApollo from '../utils/withApollo';
 import login from './login';
 import Modal from 'react-modal'
+import { CreateTray } from '../components/CreateTray';
+import { Header } from '../components/Header';
+import { Post } from '../components/Post';
 
 Modal.setAppElement('#__next')
 
@@ -21,19 +24,30 @@ interface createPostProps {
 
 const createPost: React.FC<createPostProps> = ({}) => {
 
-  useEffect(() => {
-    router.prefetch('/dashboard')
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
+  const {data, error, loading} = usePostsQuery();
+  const router = useRouter()
   return (
-    <Modal
-        isOpen={true} // The modal should always be shown on page load, it is the 'page'
+    <div className="h-screen bg-tumblrBackground flex flex-row justify-center">
+      <Header />
+      <div className="flex flex-row w-centerFull mt-24 px-6">
+        <div className="max-w-centerLeftMax w-full">
+          <CreateTray />
+          <div className="mt-5">
+            {data?.posts.map(p => <Post post={p}/>) }
+          </div>
+        </div>
+        <div className="">
+
+        </div>
+      </div>
+      <Modal
+        isOpen={true}
         onRequestClose={() => router.push('/dashboard')}
         contentLabel="Post modal"
       >
         <CreatePostForm />
-    </Modal>
+      </Modal>
+    </div>
   );
 }
 
