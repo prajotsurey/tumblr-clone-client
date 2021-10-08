0ba50e8cccd197fdafea6810313b91c9fb13f252
prajotsurey <prajotsurey@gmail.com>
2021-10-08 08:39:53 +0530

    added hover animation to create tray

--- components/CreateTray.tsx
+++ components/CreateTray.tsx
@@ -16,18 +16,25 @@ export const CreateTray: React.FC<CreateTrayProps> = ({}) => {
           height={500} 
           alt="Picture of the author" />
       </div>
-      <div className="flex flex-row flex-grow items-center bg-white rounded-sm h-24 p-4">
+      <div className="flex flex-row flex-grow items-center bg-white rounded-sm h-24 px-7">
         {/* when this link is clicked actual url is loaded which is dashboard with query parameter 'new' */}
           {/* if that page is reloaded the browser reloads the displayed url. As a result the createPost page is reloaded*/}
           {/* https://dev.to/toomuchdesign/contextual-routing-and-modal-routes-in-next-js-18fn */}
-        <Link 
-          href="/dashboard/?new=1" //actual url
-          as="/createPost/" //displayed url
-          > 
-          <div className="text-4xl font-extrabold ">
-            Create Post
-          </div>
-        </Link>
+        <div className="group">
+          <Link 
+            href="/dashboard/?new=1" //actual url
+            as="/createPost/" //displayed url
+            >
+              <div className="flex flex-col items-center">
+              <div className="text-4xl font-bold group-hover:mb-1 group-hover:-mt-1">
+                Aa
+              </div>
+              <div className="text-md">
+                Text
+              </div>
+              </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
