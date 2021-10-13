a6d1b6cef4140e73c329ca3913ed3d9c56400691
prajotsurey <prajotsurey@gmail.com>
2021-10-13 18:36:06 +0530

    fixed header margins

--- components/Header.tsx
+++ components/Header.tsx
@@ -40,7 +40,7 @@ export const Header: React.FC<HeaderProps> = ({}) => {
             :
             <button className="large:hidden" onClick={() => {setSidebarShow(true)}}><BsList className="text-xl text-white ml-3"/></button>
           }
-        <div className="text-2xl font-extrabold ml-1 text-white">
+        <div className="text-2xl font-extrabold ml-3 text-white">
           <svg viewBox="0 0 21 36.8" width="20" height="33" fill="RGB(255,255,255)"><path d="M21 36.75h-6.2c-5.577 0-9.733-2.844-9.733-9.646V16.21H0v-5.9C5.576 8.876 7.909 4.12 8.177 0h5.79v9.354h6.757v6.856h-6.756v9.486c0 2.843 1.448 3.826 3.753 3.826h3.271L21 36.75z"></path></svg>
         </div>
         <div className="text-2xl font-extrabold ml-1 text-white large:hidden">
@@ -57,7 +57,7 @@ export const Header: React.FC<HeaderProps> = ({}) => {
               </div>
             </div>
           : 
-          <div className="w-24 mr-1">
+          <div className="w-24 mr-3">
             <button type="submit" className="p-2 rounded-sm font-default bg-tumblrBlue w-full font-bold text-center" onClick={handleLogout}>
               Logout
             </button>
